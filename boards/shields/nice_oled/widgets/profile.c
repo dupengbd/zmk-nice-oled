c7eccaefa991da0bbe46ea8136dca4a06347ee7e
dupengbd <140960825+dupengbd@users.noreply.github.com>
2026-09-11 12:10:56 +0800

    profile number: pixel_operator_mono_8 -> 16 (bigger profile digit on 0.96in OLED)

--- boards/shields/nice_oled/widgets/profile.c
+++ boards/shields/nice_oled/widgets/profile.c
@@ -38,7 +38,7 @@ static void draw_active_profile(lv_obj_t *canvas, const struct status_state *sta
 static void draw_active_profile_text(lv_obj_t *canvas, const struct status_state *state) {
     // new label_dsc
     lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono_8, LV_TEXT_ALIGN_LEFT);
+    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono_16, LV_TEXT_ALIGN_LEFT);
 
     char text[14] = {};
     snprintf(text, sizeof(text), "%d", state->active_profile_index + 1);
