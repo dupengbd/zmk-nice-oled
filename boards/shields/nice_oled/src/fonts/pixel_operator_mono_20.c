89dae8b9443492cc811096f65f67a365541cd308
dupengbd <140960825+dupengbd@users.noreply.github.com>
2026-09-11 13:12:20 +0800

    fix: font include - build has no lvgl/ subdir on include path, use lvgl.h directly

--- boards/shields/nice_oled/src/fonts/pixel_operator_mono_20.c
+++ boards/shields/nice_oled/src/fonts/pixel_operator_mono_20.c
@@ -4,11 +4,7 @@
  * Opts: --no-compress --size 20 --bpp 1 --format lvgl --font PixelOperatorMono.ttf -r 0x20-0x7F -o pixel_operator_mono_20.c
  ******************************************************************************/
 
-#ifdef LV_LVGL_H_INCLUDE_SIMPLE
 #include "lvgl.h"
-#else
-#include "lvgl/lvgl.h"
-#endif
 
 #ifndef PIXEL_OPERATOR_MONO_20
 #define PIXEL_OPERATOR_MONO_20 1
